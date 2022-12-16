4bf85c93a578314a33fb28d09a674931ccdb5af6
Sharayu-18 <77717375+Sharayu-18@users.noreply.github.com>
2022-12-16 19:45:42 +0530

    Add files via upload

---
+++ textToImageASL.py
@@ -0,0 +1,33 @@
+def switch_asl(argument):
+	switcher = {
+		'A': "C://Users//shree//Desktop//SignLangRec//textToimgASL//A.jpg",
+		'B': "C://Users//shree//Desktop//SignLangRec//textToimgASL//B.jpg",
+		'C': "C://Users//shree//Desktop//SignLangRec//textToimgASL//C.jpg",
+        'D': "C://Users//shree//Desktop//SignLangRec//textToimgASL//D.jpg",
+        'E': "C://Users//shree//Desktop//SignLangRec//textToimgASL//E.jpg",
+        'F': "C://Users//shree//Desktop//SignLangRec//textToimgASL//F.jpg",
+        'G': "C://Users//shree//Desktop//SignLangRec//textToimgASL//G.jpg",
+        'H': "C://Users//shree//Desktop//SignLangRec//textToimgASL//H.jpg",
+        'I': "C://Users//shree//Desktop//SignLangRec//textToimgASL//I.jpg",
+        'J': "C://Users//shree//Desktop//SignLangRec//textToimgASL//J.jpg",
+        'K': "C://Users//shree//Desktop//SignLangRec//textToimgASL//K.jpg",
+		'L': "C://Users//shree//Desktop//SignLangRec//textToimgASL//L.jpg",
+		'M': "C://Users//shree//Desktop//SignLangRec//textToimgASL//M.jpg",
+        'N': "C://Users//shree//Desktop//SignLangRec//textToimgASL//N.jpg",
+        'O': "C://Users//shree//Desktop//SignLangRec//textToimgASL//O.jpg",
+        'P': "C://Users//shree//Desktop//SignLangRec//textToimgASL//P.jpg",
+        'Q': "C://Users//shree//Desktop//SignLangRec//textToimgASL//Q.jpg",
+        'R': "C://Users//shree//Desktop//SignLangRec//textToimgASL//R.jpg",
+        'S': "C://Users//shree//Desktop//SignLangRec//textToimgASL//S.jpg",
+        'T': "C://Users//shree//Desktop//SignLangRec//textToimgASL//T.jpg",
+		'U': "C://Users//shree//Desktop//SignLangRec//textToimgASL//U.jpg",
+		'V': "C://Users//shree//Desktop//SignLangRec//textToimgASL//V.jpg",
+        'W': "C://Users//shree//Desktop//SignLangRec//textToimgASL//W.jpg",
+        'X': "C://Users//shree//Desktop//SignLangRec//textToimgASL//X.jpg",
+        'Y': "C://Users//shree//Desktop//SignLangRec//textToimgASL//Y.jpg",
+        'Z': "C://Users//shree//Desktop//SignLangRec//textToimgASL//Z.jpg",
+	}
+
+	return switcher.get(argument, "nothing")
+
+
---
+++ textToImageISL.py
@@ -0,0 +1,32 @@
+def switch_isl(argument):
+    switcher = {
+		'A': "C://Users//shree//Desktop//SignLangRec//textToimgISL//A.jpg",
+		'B': "C://Users//shree//Desktop//SignLangRec//textToimgISL//B.jpg",
+		'C': "C://Users//shree//Desktop//SignLangRec//textToimgISL//C.jpg",
+        'D': "C://Users//shree//Desktop//SignLangRec//textToimgISL//D.jpg",
+        'E': "C://Users//shree//Desktop//SignLangRec//textToimgISL//E.jpg",
+        'F': "C://Users//shree//Desktop//SignLangRec//textToimgISL//F.jpg",
+        'G': "C://Users//shree//Desktop//SignLangRec//textToimgISL//G.jpg",
+        'H': "C://Users//shree//Desktop//SignLangRec//textToimgISL//H.jpg",
+        'I': "C://Users//shree//Desktop//SignLangRec//textToimgISL//I.jpg",
+        'J': "C://Users//shree//Desktop//SignLangRec//textToimgISL//J.jpg",
+        'K': "C://Users//shree//Desktop//SignLangRec//textToimgISL//K.jpg",
+		'L': "C://Users//shree//Desktop//SignLangRec//textToimgISL//L.jpg",
+		'M': "C://Users//shree//Desktop//SignLangRec//textToimgISL//M.jpg",
+        'N': "C://Users//shree//Desktop//SignLangRec//textToimgISL//N.jpg",
+        'O': "C://Users//shree//Desktop//SignLangRec//textToimgISL//O.jpg",
+        'P': "C://Users//shree//Desktop//SignLangRec//textToimgISL//P.jpg",
+        'Q': "C://Users//shree//Desktop//SignLangRec//textToimgISL//Q.jpg",
+        'R': "C://Users//shree//Desktop//SignLangRec//textToimgISL//R.jpg",
+        'S': "C://Users//shree//Desktop//SignLangRec//textToimgISL//S.jpg",
+        'T': "C://Users//shree//Desktop//SignLangRec//textToimgISL//T.jpg",
+		'U': "C://Users//shree//Desktop//SignLangRec//textToimgISL//U.jpg",
+		'V': "C://Users//shree//Desktop//SignLangRec//textToimgISL//V.jpg",
+        'W': "C://Users//shree//Desktop//SignLangRec//textToimgISL//W.jpg",
+        'X': "C://Users//shree//Desktop//SignLangRec//textToimgISL//X.jpg",
+        'Y': "C://Users//shree//Desktop//SignLangRec//textToimgISL//Y.jpg",
+        'Z': "C://Users//shree//Desktop//SignLangRec//textToimgISL//Z.jpg",
+        
+	}
+
+    return switcher.get(argument, "nothing")
